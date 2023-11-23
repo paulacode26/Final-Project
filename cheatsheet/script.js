a9272d63cafc5ea70278e5c767f4b08cf7d73a6a
paulacode26 <pgperez26@gmail.com>
2023-11-23 08:46:24 -0700

    first commit

---
+++ cheatsheet/script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector("h1").style.color = "white";
+    document.querySelector("h1").style.backgroundColor = "#27005D";
+});
